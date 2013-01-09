eedc1f1333fa707cfbc066ee748c5f2818e06782
Hiroaki <oahiroaki@gmail.com>
2013-01-09 20:37:36 +0900

    0101

---
+++ c++/0101.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+void replace(string str)
+{
+	for (int i = 0; i < str.size(); i++)
+}
+
+int main()
+{
+	string before = "Hoshino", after = "Hoshina";
+	int num;
+
+	cin >> num;
+
+	for (int i = 0; i < num; i++)
+	{
+		string temp;
+		cin >> temp;
+		cout << temp << endl;
+	}
+
+	return 0;
+}
