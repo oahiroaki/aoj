3660e5dd6b17b5c7421808dceb056fea1acaf573
oahiroaki <oahiroaki@gmail.com>
2013-03-29 00:15:48 +0900

    0012,0013

--- c++/0013.cc
+++ c++/0013.cc
@@ -3,18 +3,18 @@
 
 using namespace std;
 
-int main() {
-  int n;
-  vector<int> train;
-
-  while (cin >> n) {
-    if (n) {
-      train.push_back(n);
-    }
-    else {
-      cout << train.back() << endl;
-      train.pop_back();
-    }
-  }
-  return 0;
+int main()
+{
+	int n;
+	vector<int> train;
+	while (cin >> n) {
+		if (n) {
+			train.push_back(n);
+		}
+		else {
+			cout << train.back() << endl;
+			train.pop_back();
+		}
+	}
+	return 0;
 }
